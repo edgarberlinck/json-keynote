8b6df1e6ee543c890652f0b0e8b99c6153a7c50a
Edgar Muniz Berlinck <eberlinck@mpes.mp.br>
2018-09-12 16:47:32 -0300

    Removing Shit

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,6 @@ import { Client } from '../client';
 export const LIST_KEYNOTES = 'LIST_KEYNOTES';
 
 export function fetchKeynotes () {
-    console.log('Client Call');
     const payload = Client.get('/keynote');
     return {
         type: LIST_KEYNOTES,
